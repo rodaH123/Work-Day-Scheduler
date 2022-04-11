3b7d0b92f2661e4aef1f5c1f2a022b70c0af7d73
Roro <100329632+rodaH123@users.noreply.github.com>
2022-04-10 22:17:27 -0500

    updated jscript  with button function and time-block functions

--- ChallengeJscript.js
+++ ChallengeJscript.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     //Moment.js code for current date and time
     let NowMoment = moment().format("MMMM Do YYYY");
     let displayDate = document.getElementById("currentDay");
@@ -7,4 +7,31 @@ $(document).ready(function () {
     displayDate.innerHTML = NowMoment;
     let currentHour = moment().format("HH");
 }
+)
 
+ // Button function to clear local storage and clear contents
+ $("#clearFieldsBtn").click(function (event) {
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+  });
+
+  console.log("hi")
+
+//grabs hour from each time slot and compares it to actual time
+$(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
+    
+    if (currentHour == timeDiv) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  });
+
+  
