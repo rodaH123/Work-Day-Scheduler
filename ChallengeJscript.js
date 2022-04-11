630fa127292f98ee7f4b43594e6518a9d927a814
Roro <100329632+rodaH123@users.noreply.github.com>
2022-04-10 23:01:58 -0500

    created a read me file

--- ChallengeJscript.js
+++ ChallengeJscript.js
@@ -34,4 +34,22 @@ $(".time-div").each(function () {
     }
   });
 
-  
+  //grabs values from time and value divs and saves them to local storage
+ $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
+ 
+  //retrieves items from local storage and sets them in proper places
+  $("#hour-09 .time-block").val(localStorage.getItem("09"));
+  $("#hour-10 .time-block").val(localStorage.getItem("10"));
+  $("#hour-11 .time-block").val(localStorage.getItem("11"));
+  $("#hour-12 .time-block").val(localStorage.getItem("12"));
+  $("#hour-13 .time-block").val(localStorage.getItem("13"));
+  $("#hour-14 .time-block").val(localStorage.getItem("14"));
+  $("#hour-15 .time-block").val(localStorage.getItem("15"));
+  $("#hour-16 .time-block").val(localStorage.getItem("16"));
+  $("#hour-17 .time-block").val(localStorage.getItem("17"));
+ 
---
+++ Read Me.Workday Scheduler.md
@@ -0,0 +1,4 @@
+#WorkDay Scheduler
+This challenge entailed creating day calendar with the timeslots on the left and save buttons the right.
+Updated HTML elements and CSS selectors to reflect the challenge request of having present, past and future hours.
+Additionally, used Jscript syntax to write code to show current day and navigate the work day calendar by entering notes in the time-block sections.
